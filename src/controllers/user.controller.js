5fce9b8f112c7a848e515b80d055e54ea1eebbb2
Mohab Sherif <mohab.barca@gmail.com>
2022-10-02 21:11:01 +0200

    Integrating Mysql

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,36 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import responses from "../helpers/responses.js";
+import conn from "../main.js";
+import userSchema from "../schemas/user.schema.js";
+import bcrypt from "bcrypt";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {}); // TODO get all users
-userRouter.post("/", (req, res) => {}); // TODO create a new user
+userRouter.get("/", (req, res) => {
+  const sql = "SELECT * FROM users;";
+  conn.query(sql, (err, result) => {
+    if (err) responses.badRequest(res, err);
+    return responses.success(res, "Test Get users", result);
+  });
+}); // TODO get all users
+
+userRouter.post("/", (req, res) => {
+  const { error, value } = userSchema.validate(req.body);
+  if (error) return responses.badRequest(res, error.details[0].message);
+  const { name, email, password } = value;
+  const hashedPasword = bcrypt.hashSync(
+    password,
+    parseInt(process.env.SALT_ROUNDS)
+  );
+
+  const sql = "INSERT INTO users(name,email,password) VALUES(?,?,?)";
+  conn.query(sql, [name, email, hashedPasword], (err, result) => {
+    if (err) throw err;
+    console.log("Inserted 1 row");
+    console.log(result);
+  });
+  return responses.success(res, "User Created Successfully!", value);
+});
 userRouter.put("/", (req, res) => {}); // TODO update a user
 userRouter.delete("/", (req, res) => {}); // TODO delete a user
 userRouter.post("/login", (req, res) => {}); // TODO login user
--- src/main.js
+++ src/main.js
@@ -2,16 +2,45 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import errorHandler from "./middlewares/errorHandller.js";
-
-const app = express(); // create express app
+import mysql from "mysql";
+import userRouter from "./controllers/user.controller.js";
 
 dotenv.config(); // load environment variables
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DB,
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Database is connect!");
+  conn.query("CREATE DATABASE IF NOT EXISTS store", (err, result) => {
+    if (err) throw err;
+    console.log("Database created");
+  });
+  conn.query(
+    "CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),email VARCHAR(255),password VARCHAR(255))",
+    (err, result) => {
+      if (err) throw err;
+      console.log("TABLE users is created");
+    }
+  );
+});
+
+export default conn;
+
+const app = express(); // create express app
 
 app.use(express.json()); // to parse json data
 
 app.use(morgan("dev")); // to log requests
 
 app.use(errorHandler); // to handle errors
+
+app.use("/users", userRouter);
 
 const PORT = process.env.PORT || 3000; // set port
 
