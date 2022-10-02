b6225448a044b54099eebdba25b9d8cd80f2d918
Mohab Sherif <mohab.barca@gmail.com>
2022-10-02 13:23:24 +0200

    Added errorHandler

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import errorHandler from "./src/middlewares/errorHandller.js";
 
 const app = express(); // create express app
+
 dotenv.config(); // load environment variables
 
 app.use(express.json()); // to parse json data
+
 app.use(morgan("dev")); // to log requests
+
+app.use(errorHandler); // to handle errors
 
 const PORT = process.env.PORT || 3000; // set port
 
